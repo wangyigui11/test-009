0a371eef34d822a920db6d856995a82abc84e983
xgg <244301698@qq.com>
2020-08-05 09:57:13 +0800

    增加了  Echarts  组件

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,27 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/echarts',
+    name: 'echarts',
+    component: () => import(/* webpackChunkName: "table" */ '../views/Echarts.vue')
+  },
+  {
+    path: '/bar',
+    name: 'bar',
+    component: () => import(/* webpackChunkName: "table" */ '../views/Bar.vue')
+  },
+  {
+    path: '/foo',
+    name: 'foo',
+    component: () => import(/* webpackChunkName: "table" */ '../views/Foo.vue')
+  },
+  {
+    path: '/user',
+    name: 'user',
+    // 路由独享的守卫
+    component: () => import(/* webpackChunkName: "table" */ '../views/User.vue')
+  },
+
 ]
 
 const router = new VueRouter({
@@ -22,5 +39,19 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+// // 全局前置钩子
+// router.beforeEach((to, from, next) => {
+//   if (to.name !== 'Login' && !isAuthenticated) next({ name: 'Login' })
+
+//   else next()
+// })
+
+// 全局后置钩和前置不同的是 这些钩子不会接受next函数 也不会改变导航本身
+// router.afterEach((to, from) => {
+//   //...
+// })
+
+
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,58 @@
 <template>
-  <div class="home">
-    <div id="buttons">
-       <el-button @click="change">左侧切换</el-button>
-        <!-- <el-button @click="this.$router.push({path:'home',params:{ type:"leftChecked"}})">左侧选中小卡片</el-button>
-        <el-button>左侧没选中小卡片</el-button> -->
-        <el-button>右侧选中小卡片</el-button>
-        <el-button>右侧没选中小卡片</el-button>
-    </div>
-    <div class="content"  >
-        <left-checked class="leftCheck" v-for="(item, index) in list" :key= index :isCheck=isCheck></left-checked>
-        <right-checked class="rightCheck"></right-checked>
-    </div>
+  <div>
+    <input type="text" v-model="count" />
+    <el-button @click="increment">点击增加</el-button>
+    <p>{{ nickname }}</p>
+    <p>{{ age }}</p>
+    <p>{{ gender }}</p>
+    <p>'Todos为true的length', {{ a }}</p>
+    <p>'Todos为false的length', {{ b }}</p>
+    <button @click="arrayPush">往数组中添加数据</button>
+    <br />
+    <br />
+    <input type="text" placeholder="修改nickname" v-model="nickname1" />
+    <button @click="editNickname">确定修改</button>
   </div>
-
 </template>
 
 <script>
-import LeftChecked from '@/components/LeftCard'
-import RightChecked from '@/components/RightCard'
-
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 export default {
-  data () {
+  data() {
     return {
-      list: [1],
-      isCheck:false
+      nickname1: ""
+    };
+  },
+  methods: {
+    // 通过mutation 的方式， 而非直接修改 store.state.count
+    // increment() {
+    //   this.$store.commit("increment");
+    //   console.log("this.$store.state.count", this.$store.state.count);
+    // },
+    // 触发 action 中的方法 我们使用dispatch 分发
+    increment() {
+      this.$store.dispatch("incre");
+    },
+    editNickname() {
+      console.log(this.nickname1);
+      this.$store.commit("editNickname", this.nickname1);
+    },
+    arrayPush() {
+      this.$store.commit("arrayPush");
     }
   },
-  name: 'Home',
-  components: {
-    LeftChecked,
-    RightChecked,
-  },
-  methods:{
-     change () {
-       this.isCheck = !this.isCheck
-     }
+  computed: {
+    ...mapState({
+      count: state => state.count,
+      nickname: state => state.nickname,
+      age: state => state.age,
+      gender: state => state.gender
+    }),
+    // 辅助函数
+    ...mapGetters({ a: "doneTodos", b: "falseTodos" })
   }
- 
-}
+};
 </script>
-<style lang="less" scoped>
- .home{
-   padding: 0px 200px;
- }
-  #buttons{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100px;
-    background-color: pink;
-    .el-button{
-      margin-right:20px;
-      padding: 15px 20px;
-    }
-  }
-  .content{
-    margin-top: 20px;
-    background-color: #fff;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-</style>
+
+<style></style>
