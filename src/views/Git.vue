39439cda34dc012e5312731deaf2711c52f88ca4
xgg <244301698@qq.com>
2020-08-05 10:07:39 +0800

    测试（001-）

---
+++ src/views/Git.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Ceshi001 />
+  </div>
+</template>
+
+<script>
+import Ceshi001 from '../components/Github/ceshi001'
+export default {
+  components: {
+    Ceshi001,
+  }
+}
+</script>
+
+<style>
+</style>
