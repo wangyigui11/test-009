c27ddff7b359199676d09e79569c5fac1b91ef5a
xgg <244301698@qq.com>
2020-08-05 11:05:51 +0800

    huigun

--- src/router/index.js
+++ src/router/index.js
@@ -11,33 +11,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/echarts',
-    name: 'echarts',
-    component: () => import(/* webpackChunkName: "table" */ '../views/Echarts.vue')
-  },
-  {
-    path: '/bar',
-    name: 'bar',
-    component: () => import(/* webpackChunkName: "table" */ '../views/Bar.vue')
-  },
-  {
-    path: '/foo',
-    name: 'foo',
-    component: () => import(/* webpackChunkName: "table" */ '../views/Foo.vue')
-  },
-  {
-    path: '/user',
-    name: 'user',
-    // 路由独享的守卫
-    component: () => import(/* webpackChunkName: "table" */ '../views/User.vue')
-  },
-  {
-    path: '/git',
-    name: 'git',
-    // 路由独享的守卫
-    component: () => import(/* webpackChunkName: "table" */ '../views/Git.vue')
-  },
-
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
