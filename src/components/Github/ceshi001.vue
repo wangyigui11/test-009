5be9493f8eca4f3daa3491c4a14e4763ed68f1fb
xgg <244301698@qq.com>
2020-08-05 11:04:10 +0800

    0

--- src/components/Github/ceshi001.vue
+++ src/components/Github/ceshi001.vue
@@ -1,10 +1,24 @@
 <template>
   <div>ceshi001</div>
+  <!-- 
+      分支一共有五种类型
+      master   最终发布分支， 项目中有且只有一个
+      develop  项目的开发分支, 原则上项目中有且只有一个
+      feature  功能分支, 用于开发一个新功能
+      release  预发布版本, 介于develop和master之间的一个版本  用于测试
+      hotfix   修复补丁, 用于修复master 上的bug， 直接作用于master
+
+      需要增加一个新功能的时候， 可以新建 feature分支， 新功能增加完成后， 完成feature分支, 将新功能合并到develop中， 更新develop 的代码
+    
+  -->
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+    }
+  }
 }
 </script>
 
